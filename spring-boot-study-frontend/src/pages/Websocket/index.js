ce291f100888e3081c0e063b1f636ad5f31c6f59
陈铭智 <chenmingzhi@apexsoft.com.cn>
2020-04-25 14:08:58 +0800

    界面tip + websocket 使用util调用

--- spring-boot-study-frontend/src/pages/Websocket/index.js
+++ spring-boot-study-frontend/src/pages/Websocket/index.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Row, Col, Button, Input } from "antd";
 import Stomp from "stomp-websocket";
 import SockJS from "sockjs-client";
-
+import { PageTip } from "mouse-an";
+import { connect, disconnect } from "../../utils/websocktUtil";
 export default () => {
 	const [connected, setConnected] = useState(false);
 	const [client, setClient] = useState();
@@ -10,32 +11,21 @@ export default () => {
 	const [name, setName] = useState();
 	const [sysTime, setSysTime] = useState();
 	const [sysTime2, setSysTime2] = useState();
-	const connect = () => {
-		const socket = new SockJS("http://localhost:7070/gs-guide-websocket");
-		const stompClient = Stomp.over(socket);
-		stompClient.connect({}, (frame) => {
+	const connectWebsocket = () => {
+		let subscribe = {};
+		subscribe["/topic/greetings"] = ({ body }) => setResponse(body);
+		subscribe["/topic/sysTime"] = ({ body: sysTime }) => setSysTime(sysTime);
+		subscribe["/mouse/logan/sysTime"] = ({ body: sysTime }) =>
+			setSysTime2(sysTime);
+		const success = (stompClient) => {
 			setConnected(true);
-			console.log("Connected: " + frame);
-			stompClient.subscribe("/topic/greetings", function ({ body }) {
-				setResponse(body);
-			});
-			stompClient.subscribe("/topic/sysTime", function ({ body: sysTime }) {
-				setSysTime(sysTime);
-			});
-			stompClient.subscribe("/mouse/logan/sysTime", function ({
-				body: sysTime,
-			}) {
-				setSysTime2(sysTime);
-			});
-
 			setClient(stompClient);
-		});
+		};
+		connect("http://localhost:7070/ws", { subscribe, success });
 	};
 
-	const disconnect = () => {
-		if (client !== null) {
-			client.disconnect();
-		}
+	const disconnectWebsocket = () => {
+		disconnect(client);
 		setConnected(false);
 	};
 
@@ -48,7 +38,7 @@ export default () => {
 			<Col span={24} style={{ marginBottom: 10 }}>
 				<Button
 					type={"primary"}
-					onClick={connect}
+					onClick={connectWebsocket}
 					disabled={connected}
 					style={{
 						marginRight: 20,
@@ -56,7 +46,11 @@ export default () => {
 				>
 					连接
 				</Button>
-				<Button type={"danger"} onClick={disconnect} disabled={!connected}>
+				<Button
+					type={"danger"}
+					onClick={disconnectWebsocket}
+					disabled={!connected}
+				>
 					断开连接
 				</Button>
 			</Col>
@@ -102,6 +96,23 @@ export default () => {
 					autoSize={{ maxRows: 1, minRows: 1 }}
 					value={response}
 				></Input.TextArea>
+			</Col>
+			<Col span={24}>
+				<PageTip>
+					1. 界面上有三种websocket消息
+					<br />
+					&nbsp;&nbsp;1.1 连接websocket,定时1秒钟websocket往
+					<code>/topic/sysTime</code>
+					发送系统时间
+					<br />
+					&nbsp;&nbsp;1.2. 连接websocket,定时2秒钟websocket往
+					<code>/mouse/logan/sysTime</code>
+					指定的用户<code>logan</code>发送系统时间
+					<br />
+					&nbsp;&nbsp;1.3. 连接websocket,往服务器发送<code>姓名</code>
+					,服务器返回hello,姓名
+				</PageTip>
+				<PageTip>2. 定时任务的基本使用 @EnableScheduling 和 @Scheduled</PageTip>
 			</Col>
 		</Row>
 	);
